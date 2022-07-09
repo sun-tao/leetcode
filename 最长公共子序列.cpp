f0d63d6c82c49b73d3b3cedca1ff8b63c448f823
sun <1355316300@qq.com>
2022-07-09 20:30:08 +0800

    0709 commit

--- 最长公共子序列.cpp
+++ 最长公共子序列.cpp
@@ -10,30 +10,49 @@ private:
     }
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        //naive解法，O(n^4)直接超时
-        // int result = 0;
-        // vector<vector<int>> dp(text1.size(),vector<int>(text2.size(),0));
-        // for (int i = 0 ; i < dp.size() ; i++){
-        //     for (int j = 0 ; j < dp[i].size() ; j++){
-        //         if (text1[i] == text2[j]){
-        //             if (i == 0 || j == 0){
-        //                 dp[i][j] = 1;
-        //                 result = result > dp[i][j] ? result : dp[i][j];
-        //                 continue;
-        //             }
-        //             for (int k = 0 ; k < i ; k++){
-        //                 for (int v = 0 ; v < j ;v++){
-        //                     dp[i][j] = max(dp[k][v],dp[i][j]);
-        //                 }
-        //             }
-        //             dp[i][j]++;
-        //         }
-        //         result = result > dp[i][j] ? result : dp[i][j];
-        //     }
-        // }
-        // return result;
+         //naive解法，O(n^4)直接超时
+         //仿照上一题,dp[i][j]:A中以i为结尾，B中以j为结尾的最长公共子序列
+         //由于本题的公共子序列只需要相对顺序不变即可，而不需要连续，因此，判定公共子序列不仅仅取决于相邻的前一个
+         //是否相等，需要考虑i，j前面所有的组合(结尾)所对应的最长公共子序列，取最大的那个即为dp[i][j],这导致时间复杂度
+         //直接上升为O(n^4),超时
+         int result = 0;
+         vector<vector<int>> dp(text1.size(),vector<int>(text2.size(),0));
+         for (int i = 0 ; i < dp.size() ; i++){
+             for (int j = 0 ; j < dp[i].size() ; j++){
+                 if (text1[i] == text2[j]){
+                     if (i == 0 || j == 0){
+                         dp[i][j] = 1;
+                         result = result > dp[i][j] ? result : dp[i][j];
+                         continue;
+                     }
+                     for (int k = 0 ; k < i ; k++){
+                         for (int v = 0 ; v < j ;v++){
+                             dp[i][j] = max(dp[k][v],dp[i][j]);
+                         }
+                     }
+                     dp[i][j]++;
+                 }
+                 result = result > dp[i][j] ? result : dp[i][j];
+             }
+         }
+         return result;
+    }
+};
 
-        //dp[i][j]:A中以i为长度，B中以j为长度的最长公共子序列
+class Solution {
+private:
+    void printDp(const vector<vector<int>>& dp){
+        for (int i = 0 ; i < dp.size() ; i++){
+            for (int j = 0 ; j < dp[i].size() ;j++){
+                cout << dp[i][j] << ",";
+            }
+            cout << endl;
+        }
+    }
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        //dp[i][j]:A中以i为长度，B中以j为长度的最长公共子序列，因为本题不需要考虑连续，因此可以将前面的所有看做一个整体
+        //只要有相等即可
         vector<vector<int>> dp(text1.size()+1,vector<int>(text2.size()+1,0));
         //dp数组初始化,好像没必要
         for (int i = 0 ; i <= text1.size() ; i++){
