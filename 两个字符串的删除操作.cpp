ff6f4c956b8d7c46ddcd421c645d189cdb19bb50
sun <1355316300@qq.com>
2022-07-16 19:46:33 +0800

    0716 commit

--- 两个字符串的删除操作.cpp
+++ 两个字符串的删除操作.cpp
@@ -20,3 +20,30 @@ public:
         return result;
     }
 };
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        //直接法,体现了删除字符串的dp思路
+        //dp[i][j]:以i为长度的word1，以j为长度的word2，所需要的最小步数
+        vector<vector<int>> dp(word1.size()+1,vector<int>(word2.size()+1,0));
+        for (int i = 0 ; i < dp.size() ; i++){
+            dp[i][0] = i;
+        }
+        for (int j = 0 ; j < dp[0].size() ; j++){
+            dp[0][j] = j;
+        }
+
+        for (int i = 1 ; i < dp.size() ; i++){
+            for (int j = 1 ; j < dp[i].size() ; j++){
+                if (word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = min(dp[i-1][j],dp[i][j-1]) + 1;
+                }
+            }
+        }
+        return dp[word1.size()][word2.size()];
+    }
+}
