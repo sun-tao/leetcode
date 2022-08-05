c44fd3f4aedae3d56acda74905e58d7b0128bfd6
sun <1355316300@qq.com>
2022-08-05 13:32:19 +0800

    0805 commit

--- 链表/反转链表.java
+++ 链表/反转链表.java
@@ -8,7 +8,11 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+ 
+ //这题用虚拟头结点变得复杂了
+ //而且最好使用pre和cur,不应该使用cur和cur_next，使用cur和cur_next造成需要对头结点进行单独处理，否则出现环
 class Solution {
+    //迭代法
     public ListNode reverseList(ListNode head) {
         if (head == null){
             return head;
@@ -34,5 +38,37 @@ class Solution {
         }
         cur.next = null;
         return newHead;
+    }
+    
+    //迭代法
+    public ListNode reverseList(ListNode head){
+        //不用虚拟头结点，且用pre指针替代cur_next指针
+        ListNode pre = null;
+        ListNode cur = head;
+        while(cur != null){
+            ListNode cur_next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = cur_next;
+        }
+        return pre;
+    }
+    
+    //递归法
+    public ListNode traversal(ListNode pre, ListNode cur){
+        //边界条件
+        if (cur == null){
+            return pre;
+        } 
+        //单层逻辑
+        ListNode temp = cur.next;
+        cur.next = pre;
+        pre = cur;
+        cur = temp;
+        pre = traversal(pre,cur);
+        return pre;
+    }
+    public ListNode reverseList(ListNode head){
+        return traversal(null,head);
     }
 }
